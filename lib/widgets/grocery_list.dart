5a17f9fb8c2484ed5fd99e0616b1753bcea00194
rachit-13 <rd13499@gmail.com>
2025-05-16 02:13:07 +0530

    initial commit

---
+++ lib/widgets/grocery_list.dart
@@ -0,0 +1,154 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/grocery_item.dart';
+import 'package:shopping_list/widgets/new_item.dart';
+import 'package:http/http.dart' as http;
+
+class GroceryList extends StatefulWidget {
+  const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  List<GroceryItem> _groceryItem = [];
+  var _isLoading = true;
+  String? _error;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadItem();
+  }
+
+  void _loadItem() async {
+    final url = Uri.https('shopping-list-51ddc-default-rtdb.firebaseio.com',
+        'shopping-list.json');
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = 'Failed to fetch data. Please try later!';
+        });
+      }
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+        return;
+      }
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere(
+                (catItem) => catItem.value.title == item.value['category'])
+            .value;
+        loadedItems.add(
+          GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category,
+          ),
+        );
+      }
+      setState(() {
+        _groceryItem = loadedItems;
+      });
+    } catch (error) {
+      setState(() {
+        _error = 'Something went wrong! Please try again later';
+      });
+    }
+  }
+
+  void _addItem() async {
+    final newItem = await Navigator.of(context).push<GroceryItem>(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+    if (newItem == null) {
+      return;
+    }
+    setState(() {
+      _groceryItem.add(newItem);
+      _isLoading = false;
+    });
+  }
+
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItem.indexOf(item);
+    setState(() {
+      _groceryItem.remove(item);
+    });
+
+    final url = Uri.https('shopping-list-51ddc-default-rtdb.firebaseio.com',
+        'shopping-list/${item.id}.json');
+    final response = await http.delete(url);
+
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItem.insert(index, item);
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = const Center(
+      child: Text('No items added yet!'),
+    );
+
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (_groceryItem.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItem.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          key: ValueKey(_groceryItem[index].id),
+          onDismissed: (direction) {
+            _removeItem(_groceryItem[index]);
+          },
+          child: ListTile(
+            title: Text(_groceryItem[index].name),
+            leading: Container(
+              height: 24.0,
+              width: 24.0,
+              color: _groceryItem[index].category.color,
+            ),
+            trailing: Text(_groceryItem[index].quantity.toString()),
+          ),
+        ),
+      );
+    }
+
+    if (_error != null) {
+      content = Center(
+        child: Text(_error!),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(title: const Text('Your Categories'), actions: [
+        IconButton(
+          onPressed: _addItem,
+          icon: const Icon(Icons.add),
+        )
+      ]),
+      body: content,
+    );
+  }
+}
